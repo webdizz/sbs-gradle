0f232e50384d7b2f1ae43aad7b3d7449456fdc6b
Izzet Mustsfaiev <webdizz@gmail.com>
2014-10-31 01:13:48 +0200

    Add integration tests support

--- common/src/integTest/java/name/webdizz/training/gradle/SampleJavaIntegrationTest.java
+++ common/src/integTest/java/name/webdizz/training/gradle/SampleJavaIntegrationTest.java
@@ -2,11 +2,9 @@ package name.webdizz.training.gradle;
 
 import org.junit.Test;
 
-public class SampleJavaTest {
+public class SampleJavaIntegrationTest {
 
     @Test
     public void shouldTestSomething() throws Exception {
-
     }
-
 }
